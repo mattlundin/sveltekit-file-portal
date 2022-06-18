6882c0225b2827a8e71262056c251c76bd529bb3
mattlundin <74123405+mattlundin@users.noreply.github.com>
2022-06-17 22:31:04 -0500

    header gradient

--- src/lib/components/layout/header.svelte
+++ src/lib/components/layout/header.svelte
@@ -13,16 +13,10 @@
 		height: 150px;
 		width: 100%;
 		margin: 0 auto;
-		background-image: radial-gradient(
-			circle 976px at 51.2% 51%,
-			rgba(11, 27, 103, 1) 0%,
-			rgba(16, 66, 157, 1) 0%,
-			rgba(11, 27, 103, 1) 17.3%,
-			rgba(11, 27, 103, 1) 58.8%,
-			rgba(11, 27, 103, 1) 71.4%,
-			rgba(16, 66, 157, 1) 100.2%,
-			rgba(187, 187, 187, 1) 100.2%
-		);
+		background: #01025f;
+		background: -webkit-linear-gradient(top left, #01025f, #0188d5);
+		background: -moz-linear-gradient(top left, #01025f, #0188d5);
+		background: linear-gradient(to bottom right, #01025f, #0188d5);
 	}
 	.header h1 {
 		text-align: center;
